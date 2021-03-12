ecd3b820460b76796408e9f9204b275a16b97a67
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-12 21:26:03 +0900

    Create empathy-controller

--- app/controllers/empathies_controller.rb
+++ app/controllers/empathies_controller.rb
@@ -1,7 +1,13 @@
 class EmpathiesController < ApplicationController
   def create
+    empathy = current_user.empathies.build(post_id: params[:post_id])
+    empathy.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    empathy = Empathy.find_by(post_id: params[:post_id], user_id: current_user.id)
+    empathy.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
