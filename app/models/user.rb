f9a9380750524fa3a93ee5d615f3b231c63cff48
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-12 20:56:38 +0900

    Set routing and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :position
   has_many :posts
+  has_many :empathies
 
   validates :nickname, presence: true
   validates :profile, length: { maximum: 200 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users ,only: [:show]
-  resources :posts
+  resources :posts do
+    resource :empathies, only: [:create, :destroy]
+  end
 end
