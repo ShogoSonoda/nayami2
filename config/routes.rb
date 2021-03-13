d4f273630289e013bc57e8b5297117bc365f19c2
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-13 20:02:42 +0900

    Set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
+
   root to: "posts#index"
+  
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
   resources :users ,only: [:show] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
   end
+
   resources :posts do
     resource :empathies, only: [:create, :destroy]
     resources :comments, only: [:new, :create, :destroy]
   end
+
+  resources :messages, only: [:create]
+
+  resources :rooms, only: [:create, :show]
 end
