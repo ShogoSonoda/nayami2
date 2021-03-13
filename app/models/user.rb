a4bed27c1f64a7e54671b828b7ff4b683c8af606
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-13 16:12:48 +0900

    Create Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   has_many :empathies, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   validates :nickname, presence: true
   validates :profile, length: { maximum: 200 }
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 end
