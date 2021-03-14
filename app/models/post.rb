03ac55518553df6d03d8635b3e866d6c9448be4a
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-14 15:20:05 +0900

    Create notification method

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,4 +27,40 @@ class Post < ApplicationRecord
   def empathy_by?(user)
     empathies.where(user_id: user.id).exists?
   end
+
+  def create_notification_empathy!(current_user)
+    temp = Notification.where(["visitor_id = ? and vidited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'empathy'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        vidited_id: user_id,
+        action: 'empathy'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_userm comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      vidited_id: vidited_id,
+      action: 'comment'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.checked if notification.valid?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,15 @@ class User < ApplicationRecord
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
+
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ?, current_user.id, id, 'follow"])
+    if temp.blank?
+      Notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
