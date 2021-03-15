aa7810f09f00a31eab5329d646868775edd0a8e1
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-15 21:13:30 +0900

    Resize avator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :position
   has_many :posts
