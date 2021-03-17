7ebc0a491d21b7fb1da762fc6418a95242d7cf2e
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-17 16:23:03 +0900

    Create front design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
     unless @user.id == current_user.id
