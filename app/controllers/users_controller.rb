4bdc6b0ac88aa7d6090987b1921a50d322b679a5
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-18 15:30:07 +0900

    Fix filtaling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :search, :follows, :followers]
   before_action :set_user, only: [:show, :follows, :followers]
 
   def show
