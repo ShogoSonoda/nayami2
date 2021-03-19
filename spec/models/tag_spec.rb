19d232d4354f1406090a7085dfe7dff0e61d2b57
ShogoSonoda <sonoponsyougo@gmail.com>
2021-03-19 20:34:44 +0900

    Create tag test

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = Tag.create(name: "転職")
+  end
+  context 'タグを保存できる場合' do
+    it "nameがあればタグ登録できる" do
+      expect(@tag).to be_valid
+    end
+  end
+
+  context 'タグを保存できない場合' do
+    it "nameがないとタグ登録できない" do
+      @tag.name = ""
+      @tag.valid?
+      expect(@tag).to be_invalid
+    end
+    
+    it "nameが51文字以上だとタグ登録できない" do
+      @tag.name = "あ" * 51
+      @tag.valid?
+      expect(@tag).to be_invalid
+    end
+
+    it "同じnameは保存されない" do
+      another_tag = Tag.new
+      another_tag.name = @tag.name
+      another_tag.save
+      another_tag.valid?
+      expect(another_tag).to be_invalid
+    end
+  end 
 end
