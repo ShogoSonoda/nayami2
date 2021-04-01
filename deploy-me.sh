c5faed12ca9ef54d677b83a4780658f2dd81fee1
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-01 11:16:59 +0900

    バックスラッシュの追記

--- deploy-me.sh
+++ deploy-me.sh
@@ -2,6 +2,6 @@
 
 cd /var/www/nayami2/ \
 && git pull \
-&& bundle exec rails assets:precompile RAILS_ENV=production
+&& bundle exec rails assets:precompile RAILS_ENV=production \
 && sudo service nginx start \
 && bundle exec rails s -e production
