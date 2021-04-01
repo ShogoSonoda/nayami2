8f7be12da9f3a2ec3cc373e6cd89a7b8bcc207b6
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-01 11:30:57 +0900

    deploy-me.shの修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 cd /var/www/nayami2/ \
-&& git pull \
-&& bundle exec rails assets:precompile RAILS_ENV=production \
-&& sudo service nginx start \
-&& bundle exec rails s -e production
+&& git pull
