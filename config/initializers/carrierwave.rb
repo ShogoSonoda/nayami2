db6c5508b502f3d2bdcec1bad1adace833ae5577
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-02 17:54:00 +0900

    cloudfrontを使うために、carrierwaveの設定を更新

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory = 'nayami2-avatar'
+  config.asset_host = 'https://static.www.nayami2app.com'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
