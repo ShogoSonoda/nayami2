6254fb433d354b63c28131d9a1c5d80da58a7031
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-08 10:55:49 +0900

    いいね機能の非同期化

--- app/controllers/empathies_controller.rb
+++ app/controllers/empathies_controller.rb
@@ -1,17 +1,22 @@
 class EmpathiesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :set_post, only: [:create, :destroy]
 
   def create
     empathy = current_user.empathies.build(post_id: params[:post_id])
     empathy.save
-    post = Post.find(params[:post_id])
-    post.create_notification_empathy!(current_user)
-    redirect_back(fallback_location: root_path)
+    @post.create_notification_empathy!(current_user)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     empathy = Empathy.find_by(post_id: params[:post_id], user_id: current_user.id)
     empathy.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
