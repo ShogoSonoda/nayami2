a9d59cf8475970e8ca1d311e3f4401a3030af9d6
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-09 08:43:04 +0900

    ガード節を導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,27 +2,27 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.new(message_params)
-      @room = @message.room
-      if @message.save
-        @roomMemberNotMe = Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
-        @theId = @roomMemberNotMe.find_by(room_id: @room.id)
-        notification = current_user.active_notifications.new(
-          room_id: @room.id,
-          message_id: @message.id,
-          visited_id: @theId.user_id,
-          visitor_id: current_user.id,
-          action: 'dm'
-        )
-        if notification.visitor_id == notification.visited_id
-          notification.checked = true
-        end
-        notification.save if notification.valid?
-        respond_to do |format|
-          format.html { redirect_back(fallback_location: root_path) }
-          format.js
-        end
+    return unless Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+    
+    @message = Message.new(message_params)
+    @room = @message.room
+    if @message.save
+      @roomMemberNotMe = Entry.where(room_id: @room.id).where.not(user_id: current_user.id)
+      @theId = @roomMemberNotMe.find_by(room_id: @room.id)
+      notification = current_user.active_notifications.new(
+        room_id: @room.id,
+        message_id: @message.id,
+        visited_id: @theId.user_id,
+        visitor_id: current_user.id,
+        action: 'dm'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+      respond_to do |format|
+        format.html { redirect_back(fallback_location: root_path) }
+        format.js
       end
     end
   end
