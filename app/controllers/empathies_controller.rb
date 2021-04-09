8b829b98a0ebb9ccf1adaa7db0840e762e9df2ef
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-09 22:35:46 +0900

    respond toを追加

--- app/controllers/empathies_controller.rb
+++ app/controllers/empathies_controller.rb
@@ -3,16 +3,22 @@ class EmpathiesController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
 
   def create
-    empathy = current_user.empathies.build(post_id: params[:post_id])
-    empathy.save
+    empathy = current_user.empathies.create(post_id: params[:post_id])
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
+
     @post.create_notification_empathy!(current_user)
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     empathy = Empathy.find_by(post_id: params[:post_id], user_id: current_user.id)
     empathy.destroy
-    # redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   private
