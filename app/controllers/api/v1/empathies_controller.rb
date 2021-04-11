321eea55c12299fd551823b24c1cfc2b18852eec
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-11 14:35:28 +0900

    いいね機能のコントローラーを定義

---
+++ app/controllers/api/v1/empathies_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class EmpathyController < ApplicationController
+      skip_forgery_protection
+
+      def create
+        empathy = Empathy.create!(user_id: current_user.id ,post_id: params[:post_id])
+        render json: { empathy_id: empathy.id }
+      end
+
+      def destroy
+        Empathy.find(params[:id]).destroy!
+        render json: {}
+      end
+    end
+  end
+end
