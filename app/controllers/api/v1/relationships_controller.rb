48461147d7c47d88403d3b80c7cb278c7bda75b4
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-19 17:27:36 +0900

    APIコントローラーの設定

---
+++ app/controllers/api/v1/relationships_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class RelationshipsController < ApplicationController
+
+      def create
+        followed_user = User.find(params[:followed_id])
+        relationship = current_user.follow(followed_user)
+        render json: { relationship_id: relationship.id }
+      end
+
+      def destroy
+        Relationship.find(params[:id]).destroy!
+        render json: { }
+      end
+    end
+  end
+end
+　
