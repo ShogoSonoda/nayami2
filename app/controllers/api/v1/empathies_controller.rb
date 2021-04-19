819f155808fb480271723d674d19e37601bf2a4c
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-19 16:25:41 +0900

    投稿に共感されたら、通知がいくようにする

--- app/controllers/api/v1/empathies_controller.rb
+++ app/controllers/api/v1/empathies_controller.rb
@@ -6,6 +6,8 @@ module Api
       def create
         empathy = current_user.empathies.create!(post_id: params[:post_id])
         render json: { empathy_id: empathy.id }
+        @post = Post.find(params[:post_id])
+        @post.create_notification_empathy!(current_user)
       end
 
       def destroy
