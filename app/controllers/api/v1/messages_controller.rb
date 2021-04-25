81439b35e28784acaf0733852dbbfe1ab58f6e3b
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-25 22:07:09 +0900

    ユーザーのニックネームをjsonで返すように設定した

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,7 +3,7 @@ module Api
     class MessagesController < ApplicationController
       def create
         message = current_user.messages.create!(room_id: params[:room_id], text: params[:text])
-        render json: { message: message }
+        render json: { message: message, user_name: current_user.nickname }
       end
     end
   end
