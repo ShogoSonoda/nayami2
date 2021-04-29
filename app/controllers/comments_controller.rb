542114182a97a3c6b727e59b68953bbaa09cb534
ShogoSonoda <sonoponsyougo@gmail.com>
2021-04-29 11:23:11 +0900

    投稿詳細ページ内にてコメントを送信できるように修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,15 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
 
-  def new
-    @post = Post.find(params[:post_id])
-    @comment = Comment.new
-  end
-
   def create
-    @comment = Comment.new(comment_params)
-    @post = @comment.post
-    if @comment.save
-      @post.create_notification_comment!(current_user, @comment.id)
-      redirect_to post_path(params[:post_id])
+    comment = Comment.new(comment_params)
+    post = comment.post
+    if comment.save
+      post.create_notification_comment!(current_user, comment.id)
+      redirect_back(fallback_location: root_path) 
     else
-      render :new
+      # エラー文を表示させる
+      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -26,6 +22,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:text, :post_id).merge(user_id: current_user.id)
   end
 end
